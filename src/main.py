08f4dfb35ab38df34e34377c86e288d574416b28
Bgoodwin24 <bgoodwin24@gmail.com>
2025-02-25 21:05:16 -0800

    feat: Update Static Site Generator
    - Add more markdown files to content dir
    - Add more images to static/images
    - Add image to public/images
    - Update main.py to use system arguements instead of hard coded paths
    - Add basepath variable to use sys.argv with error handling
    - Update generate_page and generate_pages_recursively to use basepath argument and replace all href and src instances with basepath
    - Create build.sh to build site for production
    - Update main.py to build site into docs instead of public
    - Removed public directory from project and gitignore
    - Change Github page to use main branch and /docs as defaults to auto deploy to github page when something is in it

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 import os
 import shutil
+import sys
 from gencontent import generate_pages_recursive
 from copystatic import copy_files_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
-
 def main():
+	basepath = sys.argv
+
+	if len(sys.argv) < 2:
+		basepath = "/"
+	else:
+		basepath = sys.argv[1]
+
 	print("Deleting public directory...")
 	if os.path.exists(dir_path_public):
 		shutil.rmtree(dir_path_public)
@@ -26,7 +33,7 @@ def main():
 		return
 	
 	print("Generating page...")
-	generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+	generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 	print("Page generation complete!")
 
